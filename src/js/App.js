3a6408389573a26ca98f79caf7ba378b44113e2d
vivek bhansali <vivekbhansali87@gmail.com>
2020-05-27 19:02:33 +0530

    initial commit

---
+++ src/js/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <React.StrictMode>
+      <header>
+      </header>
+
+      <main>
+      </main>
+
+      <footer>
+      </footer>
+    
+
+    </React.StrictMode>
+  );
+}
+
+export default App;
